e64859c390239b82b4225fa001a4e5579c1a81c6
Samridh <sb.samridhbhau@gmail.com>
2025-04-02 23:34:58 +0530

    concurrent

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <netdb.h>
 #include <errno.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define MAX_LENGTH 4096
+
+void *handle_request_thread(void *);
 
 int main() {
   int server_fd;
@@ -29,32 +33,46 @@ int main() {
     return 1;
   }
 
-  int backlog = 4;
-  if (listen(server_fd, backlog) == -1){
+  if (listen(server_fd, SOMAXCONN) == -1){
     printf("Listen Failed: %s\n", strerror(errno));
     return 1;
   }
 
-  struct sockaddr_storage client_addr;
-  socklen_t addr_size = sizeof client_addr;
 
-  int client_fd;
-  if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_size)) == -1 ){
-    printf("Accept Failed: %s\n", strerror(errno));
-    return 1;
+  while(1){
+    struct sockaddr_storage client_addr;
+    socklen_t addr_size = sizeof client_addr;
+
+    long long int client_fd;
+    if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_size)) == -1 ){
+      printf("Accept Failed: %s\n", strerror(errno));
+      return 1;
+    }
+
+    pthread_t thread;
+    pthread_create(&thread, NULL, handle_request_thread, (void *)client_fd);
   }
 
-  printf("Client Connected!\n");
+  close(server_fd);
+  return 0;
+}
+
+void *handle_request_thread(void *fd){
+  int client_fd = (long long int)fd;
+  int err = 1;
 
   char buf[MAX_LENGTH];
 
-  if (recv(client_fd, buf, MAX_LENGTH, 0) <= 0){
+  int bytes_received;
+  if ( (bytes_received = recv(client_fd, buf, MAX_LENGTH - 1, 0)) <= 0){
     printf("Recv Failed: %s\n", strerror(errno));
-    return 1;
+    return NULL;
   }
 
-  char *token = strtok(buf, " ");
-  token = strtok(NULL, " ");
+  int buf_len = strlen(buf);
+  char *saveptr;
+  char *token = strtok_r(buf, " ", &saveptr);
+  token = strtok_r(NULL, " ", &saveptr);
 
   if (strcmp(token, "/") == 0){
     char *buf = "HTTP/1.1 200 OK\r\n\r\n";
@@ -62,66 +80,50 @@ int main() {
 
     if (send(client_fd, buf, len, 0) == -1){
       printf("Send Failed: %s\n", strerror(errno));
-      return 1;
+      return NULL;
     }
   }
   else if (strncmp(token,"/echo/", 6) == 0){
-    char *str = strtok(NULL, "/");
-    char buf[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ";
+    char *str = token + 6;
 
     // Content-Length
-    int body_len = strlen(str);
-    char str_len[body_len];
-    snprintf(str_len, body_len, "%d\r\n\r\n", body_len);
-    strcat(buf, str_len);
-
-    // Response
-    strcat(buf, str);
-
-    int len = strlen(buf);
-
-    if (send(client_fd, buf, len, 0) == -1){
-      printf("Send Failed: %s\n", strerror(errno));
-      return 1;
-    }
-  }
-  else if (strcmp(token, "/user-agent") == 0){
-    //TODO Review code
-    char *ua = strtok(NULL, "\r\n");
-    ua = strtok(NULL, "\r\n");
-    ua = strtok(NULL, "\r\n");
-    char *str = strtok(ua, " ");
-    str = strtok(NULL, " ");
-
-    char res[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ";
-
-
-    int body_len = strlen(str);
-    char str_len[body_len];
-    snprintf(str_len, body_len, "%d\r\n\r\n", body_len);
-
-    strcat(res, str_len);
-    strcat(res, str);
-    printf("%s\n", res);
+    char res[MAX_LENGTH];
+    sprintf(res, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s",            strlen(str), str);
 
     int len = strlen(res);
+
+    if (send(client_fd, res, len, 0) == -1){
+      printf("Send Failed: %s\n", strerror(errno));
+      return NULL;
+    }
+  }
+  else if (strncmp(token, "/user-agent", 11) == 0){
+    //TODO Review code
+    strtok_r(NULL, "\r\n", &saveptr);
+    strtok_r(NULL, "\r\n", &saveptr);
+    char *str = strtok_r(NULL, "\r\n", &saveptr)+ 12;
+
+    char res[MAX_LENGTH];
+    sprintf(res, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s",
+            strlen(str), str);
+
+    int len = strlen(res);
+
     if (send(client_fd, res, len, 0) == -1){
       printf("Send failed: %s\n", strerror(errno));
-      return 1;
+      return NULL;
     }
   }
   else {
-    char *buf = "HTTP/1.1 404 Not Found\r\n\r\n";
-    int len = strlen(buf);
+    char *res= "HTTP/1.1 404 Not Found\r\n\r\n";
+    int len = strlen(res);
 
-    if (send(client_fd, buf, len, 0) == -1){
+    if (send(client_fd, res, len, 0) == -1){
       printf("Send Failed: %s\n", strerror(errno));
-      return 1;
+      return NULL;
     }
   }
 
-  
-  close(server_fd);
-
+  close(client_fd);
   return 0;
 }
