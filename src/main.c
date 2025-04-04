1d725b795fdd1ea0b1b3c38d4f56feaf459b2451
sam <sb.samridhbhau@gmail.com>
2025-04-04 23:51:07 +0530

    files endpoint

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <netdb.h>
+#include <assert.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -10,8 +11,17 @@
 #define MAX_LENGTH 4096
 
 void *handle_request_thread(void *);
+void *handle_file_request(void *);
+void *handle_not_found(void *);
 
-int main() {
+struct client_info {
+  long long int client_fd;
+  char **args;      
+  int arg_len;
+  char *filename;   // requested file
+};
+
+int main(int argc, char *argv[]) {
   int server_fd;
   
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -24,7 +34,7 @@ int main() {
   
   struct sockaddr_in serv_addr = {
     .sin_family = AF_INET,
-    .sin_port = htons(3221),
+    .sin_port = htons(4221),
     .sin_addr = { htonl(INADDR_ANY) }
   };
 
@@ -38,7 +48,6 @@ int main() {
     return 1;
   }
 
-
   while(1){
     struct sockaddr_storage client_addr;
     socklen_t addr_size = sizeof client_addr;
@@ -49,22 +58,29 @@ int main() {
       return 1;
     }
 
+    struct client_info *c_info = (struct client_info*)malloc(sizeof(*c_info));
+    c_info->client_fd = client_fd;
+    c_info->args = argv;
+    c_info->arg_len = argc;
+
     pthread_t thread;
-    pthread_create(&thread, NULL, handle_request_thread, (void *)client_fd);
+    pthread_create(&thread, NULL, handle_request_thread, c_info);
+    pthread_detach(thread);
   }
 
   close(server_fd);
   return 0;
 }
 
-void *handle_request_thread(void *fd){
-  int client_fd = (long long int)fd;
+void *handle_request_thread(void *c){
+  struct client_info* c_info = (struct client_info*)c;
+  long long int client_fd = c_info->client_fd;
   int err = 1;
 
   char buf[MAX_LENGTH];
 
   int bytes_received;
-  if ( (bytes_received = recv(client_fd, buf, MAX_LENGTH - 1, 0)) <= 0){
+  if ( (bytes_received = recv(client_fd, buf, MAX_LENGTH, 0)) <= 0){
     printf("Recv Failed: %s\n", strerror(errno));
     return NULL;
   }
@@ -90,9 +106,7 @@ void *handle_request_thread(void *fd){
     char res[MAX_LENGTH];
     sprintf(res, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s",            strlen(str), str);
 
-    int len = strlen(res);
-
-    if (send(client_fd, res, len, 0) == -1){
+    if (send(client_fd, res, strlen(res) + 1, 0) == -1){
       printf("Send Failed: %s\n", strerror(errno));
       return NULL;
     }
@@ -104,26 +118,86 @@ void *handle_request_thread(void *fd){
     char *str = strtok_r(NULL, "\r\n", &saveptr)+ 12;
 
     char res[MAX_LENGTH];
-    sprintf(res, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s",
+    sprintf(res,"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s",
             strlen(str), str);
 
-    int len = strlen(res);
-
-    if (send(client_fd, res, len, 0) == -1){
+    if (send(client_fd, res, strlen(res) + 1, 0) == -1){
       printf("Send failed: %s\n", strerror(errno));
       return NULL;
     }
   }
-  else {
-    char *res= "HTTP/1.1 404 Not Found\r\n\r\n";
-    int len = strlen(res);
-
-    if (send(client_fd, res, len, 0) == -1){
-      printf("Send Failed: %s\n", strerror(errno));
-      return NULL;
-    }
+  else if(strncmp(token, "/files/", 7) == 0){
+    char filename[strlen(token) - 7];
+    strcpy(filename, token+7);
+    c_info->filename = filename;
+    handle_file_request(c);
+  }
+  else{
+    handle_not_found((void *)c_info);
   }
 
+  free(c_info);
+  shutdown(client_fd, SHUT_WR);
   close(client_fd);
+  return 0;
+}
+
+void *handle_file_request(void *c){
+  struct client_info *c_info = (struct client_info *)c;
+  long long int client_fd = c_info->client_fd;
+  char *filename = c_info->filename;
+  char **args = c_info->args;
+  int arg_len = c_info->arg_len;
+  char *dir;
+
+  if (arg_len > 1 && strcmp(args[1], "--directory") == 0){
+    dir = args[2];
+  }
+  if (dir == NULL)
+    return NULL;
+  
+  int len = strlen(dir) + strlen(filename) + 2;
+  char path[len];
+  snprintf(path, len,"%s/%s", dir, filename);
+
+  char s[MAX_LENGTH];
+  pthread_mutex_t lock;
+  int rc = pthread_mutex_init(&lock, NULL);
+  
+  FILE *fptr = fopen(path, "r");
+
+  if (fptr == NULL){
+    handle_not_found(c_info);
+    return 0;
+  }
+  
+  pthread_mutex_lock(&lock);
+  // Critical Section
+  while ((fgets(s, sizeof s, fptr)) != NULL){
+     continue;
+  }
+  fclose(fptr);
+  pthread_mutex_unlock(&lock);
+
+  char res[MAX_LENGTH];
+
+  sprintf(res, "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %ld\r\n\r\n%s", strlen(s), s);
+
+  if (send(client_fd, res, strlen(res) + 1, 0) == -1){
+      printf("Send Failed: %s\n", strerror(errno));
+      return NULL;
+  }
+  return 0;
+}
+
+void *handle_not_found(void *c){
+  char *res = "HTTP/1.1 404 Not Found\r\n\r\n";
+  int len = strlen(res);
+  long long int client_fd = ((struct client_info*)c)->client_fd;
+
+  if (send(client_fd, res, len, 0) == -1){
+      printf("Send Failed: %s\n", strerror(errno));
+      return NULL;
+  }
   return 0;
 }
