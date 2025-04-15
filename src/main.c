a6d567786975078bf576048986f23308581a72f2
sam <sb.samridhbhau@gmail.com>
2025-04-15 19:14:10 +0530

    persistent connections

--- src/main.c
+++ src/main.c
@@ -18,6 +18,7 @@ static char* directory;
 
 struct client_info {
   long long int client_fd;
+  int close_connection;
 };
 
 struct header {
@@ -37,14 +38,18 @@ struct request_data {
   struct header_data* header_data;
 };
 
-void *handle_file_request(void *, struct request_data*);
-void *write_file(char *, char *);
-void parse_request_data(char *buf, struct request_data*);
+
 void *handle_request_thread(void *);
-void *handle_not_found(void *);
+int handle_file_request(void *, struct request_data*);
+int handle_not_found(void *);
+int handle_echo(struct client_info* c_info, char *echo_str, struct request_data *req_data);
+int handle_user_agent(struct client_info* c_info, struct request_data *req_data);
+
+// utility
 char *read_file(char *);
+void parse_request_data(char *buf, struct request_data*);
+void *write_file(char *, char *);
 char **split_string(char *, char *,int *);
-void send_message(long long int, char *);
 unsigned char *compress_zlib(char *, int*);
 
 int main(int argc, char *argv[]) {
@@ -61,7 +66,7 @@ int main(int argc, char *argv[]) {
   int reuse = 1;
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof reuse) == -1){
     printf("setsockopt Failed: %s\n", strerror(errno));
-    return 1;
+    return 0;
   }
 
   struct sockaddr_in serv_addr = {
@@ -97,6 +102,7 @@ int main(int argc, char *argv[]) {
 
     struct client_info *c_info = (struct client_info*)malloc(sizeof(*c_info));
     c_info->client_fd = client_fd;
+    c_info->close_connection = 0;
 
     pthread_create(&thread, &thread_attr, handle_request_thread, c_info);
   }
@@ -109,108 +115,60 @@ int main(int argc, char *argv[]) {
 void *handle_request_thread(void *c){
   struct client_info* c_info = (struct client_info*)c;
   long long int client_fd = c_info->client_fd;
-  int err = 1;
-
-  char client_req[MAX_LENGTH];
-
-  int bytes_received;
-  if ( (bytes_received = recv(client_fd, client_req, MAX_LENGTH, 0)) <= 0){
-    printf("Recv Failed: %s\n", strerror(errno));
-    return NULL;
-  }
-
   struct request_data *req_data = (struct request_data *)malloc(sizeof(struct request_data));
-  parse_request_data(client_req, req_data);
 
-  if (strcmp(req_data->request_path, "/") == 0){
-    char *buf = "HTTP/1.1 200 OK\r\n\r\n";
-    int len = strlen(buf);
-
-    if (send(client_fd, buf, len, 0) == -1){
-      printf("Send Failed: %s\n", strerror(errno));
-      return NULL;
+  while (1){
+    char client_req[MAX_LENGTH];
+    int bytes_received;
+    while ( (bytes_received = recv(client_fd, client_req, MAX_LENGTH, 0)) <= 0){
+      //printf("Recv Failed: %s\n", strerror(errno));
+      continue;
     }
-  }
-  else if (strncmp(req_data->request_path,"/echo/", 6) == 0){
-    char *str = req_data->request_path + 6;
-    size_t headers_len = req_data->header_data->length;
+
+    parse_request_data(client_req, req_data);
+
+    // check Connection header
     struct header **headers = req_data->header_data->headers;
-
-    char *client_encodings;
-    for (int i = 0; i < headers_len; i++){
-      if (strcmp(headers[i]->name, "Accept-Encoding") == 0){
-        client_encodings = headers[i]->value;
+    for (int i = 0; i < req_data->header_data->length; i++){
+      if (strcmp(headers[i]->name, "Connection") == 0 && strcmp(headers[i]->value, "close") == 0){
+        c_info->close_connection = 1;
+        break;
       }
     }
 
-    char res[MAX_LENGTH] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n";
-
-    if (client_encodings == NULL){
-      char s[MAX_LENGTH];
-      sprintf(s, "Content-Length: %ld\r\n\r\n%s", strlen(str), str);
-      strcat(res, s);
-      send_message(client_fd, res);
-      return 0;
+    if (c_info->close_connection){
+      char buf[] = "HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n";
+      send(client_fd, buf, strlen(buf), 0);
+      break;
     }
 
-    int len;
-    unsigned char* c_str = compress_zlib(str, &len);
+    if (strcmp(req_data->request_path, "/") == 0){
+      char buf[] = "HTTP/1.1 200 OK\r\n\r\n";
 
-    int clients_len;
-    char **client_vals = split_string(client_encodings, ",", &clients_len);
-
-    char *encoding;
-    for (int i = 0; i < clients_len; i++){
-      encoding = client_vals[i];
-
-      if (strcmp(encoding, "gzip") == 0){
-        char s[MAX_LENGTH];
-        sprintf(s, "Content-Encoding: gzip\r\nContent-Length: %d\r\n\r\n", len);
-        strcat(res, s);
-        send_message(client_fd, res);
-        send(client_fd, c_str, len, 0);
-        return 0;
-      }
+      send(client_fd, buf, strlen(buf), 0);
     }
-    strcat(res, "\r\n");
-
-    send_message(client_fd, res);
-    return 0;
-  }
-  else if (strncmp(req_data->request_path, "/user-agent", 11) == 0){
-    char res[MAX_LENGTH];
-    size_t headers_len = req_data->header_data->length;
-    struct header **headers = req_data->header_data->headers;
-
-    char *agent_header;
-    int i;
-    for (i = 0; i < headers_len; i++){
-      if (strcmp(headers[i]->name, "user-agent") == 0 || strcmp(headers[i]->name, "User-Agent") == 0)
-      {
-        agent_header = headers[i]->value;
-      }
+    else if (strncmp(req_data->request_path,"/echo/", 6) == 0){
+      char *str = req_data->request_path + 6;
+      handle_echo(c_info, str, req_data);
     }
-    sprintf(res,"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s",
-            strlen(agent_header), agent_header);
-
-    if (send(client_fd, res, strlen(res)+1, 0) == -1){
-      printf("Send failed: %s\n", strerror(errno));
-      return NULL;
+    else if (strncmp(req_data->request_path, "/user-agent", 11) == 0){
+      handle_user_agent(c_info, req_data);
     }
-  }
-  else if (strncmp(req_data->request_path, "/files/", 7) == 0){
-    char *filename = req_data->request_path + 7;
-    handle_file_request(c_info, req_data);
-  }
-  else{
-    handle_not_found((void *)c_info);
+    else if (strncmp(req_data->request_path, "/files/", 7) == 0){
+      char *filename = req_data->request_path + 7;
+      handle_file_request(c_info, req_data);
+    }
+    else{
+      handle_not_found((void *)c_info);
+    }
+
   }
 
-  free(c_info);
   free(req_data);
+  free(c_info);
   shutdown(client_fd, SHUT_WR);
   close(client_fd);
-  return 0;
+  pthread_exit(0);
 }
 
 void parse_request_data(char *buf, struct request_data* req){
@@ -269,7 +227,7 @@ void parse_request_data(char *buf, struct request_data* req){
 
 
 
-void *handle_file_request(void *c, struct request_data* req){
+int handle_file_request(void *c, struct request_data* req){
   struct client_info *c_info = (struct client_info *)c;
   long long int client_fd = c_info->client_fd;
   char *filename = req->request_path + 7;
@@ -289,9 +247,9 @@ void *handle_file_request(void *c, struct request_data* req){
     char res[MAX_LENGTH];
     sprintf(res, "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %ld\r\n\r\n%s", strlen(s), s);
 
-    if (send(client_fd, res, strlen(res) + 1, 0) == -1){
+    if (send(client_fd, res, strlen(res), 0) == -1){
       printf("Send Failed: %s\n", strerror(errno));
-      return NULL;
+      return 0;
     }
 
     free(s);
@@ -304,24 +262,25 @@ void *handle_file_request(void *c, struct request_data* req){
 
     char res[] = "HTTP/1.1 201 Created\r\n\r\n";
 
-    if (send(client_fd, res, sizeof(res), 0) == -1){
+    if (send(client_fd, res, strlen(res), 0) == -1){
       printf("Send Failed: %s\n", strerror(errno));
-      return NULL;
+      return 0;
     }
   }
-  return 0;
+
+  return 1;
 }
 
-void *handle_not_found(void *c){
+int handle_not_found(void *c){
   char *res = "HTTP/1.1 404 Not Found\r\n\r\n";
   int len = strlen(res);
   long long int client_fd = ((struct client_info*)c)->client_fd;
 
   if (send(client_fd, res, len, 0) == -1){
     printf("Send Failed: %s\n", strerror(errno));
-    return NULL;
+    return 0;
   }
-  return 0;
+  return 1;
 }
 
 char *read_file(char *path){
@@ -395,12 +354,6 @@ char **split_string(char *string, char* delim,int *clients_len){
   return vals;
 }
 
-void send_message(long long int client_fd, char *msg){
-  if (send(client_fd, msg, strlen(msg), 0) == -1){
-    printf("Send Failed: %s\n", strerror(errno));
-  }
-}
-
 unsigned char *compress_zlib(char *msg, int* len){
   z_stream zstrm;
   int have;
@@ -426,4 +379,64 @@ unsigned char *compress_zlib(char *msg, int* len){
   deflateEnd(&zstrm);
 
   return out;
+}
+
+int handle_echo(struct client_info* c_info,char *str, struct request_data* req_data){
+  size_t headers_len = req_data->header_data->length;
+  long long client_fd = c_info->client_fd;
+  struct header **headers = req_data->header_data->headers;
+
+  char *client_encodings = NULL;
+  for (int i = 0; i < headers_len; i++){
+    if (strcmp(headers[i]->name, "Accept-Encoding") == 0){
+      client_encodings = headers[i]->value;
+    }
+  }
+
+  if (client_encodings != NULL){
+    int len;
+    unsigned char* c_str = compress_zlib(str, &len);
+    int clients_len;
+    char **client_vals = split_string(client_encodings, ",", &clients_len);
+
+    char *encoding;
+    for (int i = 0; i < clients_len; i++){
+      encoding = client_vals[i];
+
+      if (strcmp(encoding, "gzip") == 0){
+        char res[MAX_LENGTH];
+        sprintf(res, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: %d\r\n\r\n", len);
+        send(client_fd, res, strlen(res), 0);
+        send(client_fd, c_str, len, 0);
+        return 1;
+      }
+    }
+  }
+
+  char res[MAX_LENGTH];
+  sprintf(res, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s", 
+          strlen(str), str);
+  send(client_fd, res, strlen(res), 0);
+  return 1;
+}
+
+int handle_user_agent(struct client_info* c_info, struct request_data *req_data){
+  char res[MAX_LENGTH];
+  size_t headers_len = req_data->header_data->length;
+  long long client_fd = c_info->client_fd;
+  struct header **headers = req_data->header_data->headers;
+
+  char *agent_header;
+  int i;
+  for (i = 0; i < headers_len; i++){
+    if (strcmp(headers[i]->name, "user-agent") == 0 || strcmp(headers[i]->name, "User-Agent") == 0)
+    {
+      agent_header = headers[i]->value;
+    }
+  }
+  sprintf(res,"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s",
+          strlen(agent_header), agent_header);
+
+  send(client_fd, res, strlen(res), 0);
+  return 1;
 }
